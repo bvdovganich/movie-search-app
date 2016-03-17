6af65605329f8ecc16fe9116e7fadae1b02d3dcf
Богдан Довганич <bvdovganich@gmail.com>
2016-03-17 16:58:41 +0300

    Material

---
+++ Movie Search App/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Movie_Search_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List<string> ComboboxData = new List<string>();
+            
+
+        public void Combobox1_Loaded(object sender, RoutedEventArgs e)
+        {
+            ComboboxData.Add("Full Title");
+            ComboboxData.Add("Year");
+            ComboboxData.Add("Director");
+            ComboboxData.Add("Actors");
+            var comboBox = sender as ComboBox;
+            comboBox.ItemsSource = ComboboxData;
+            comboBox.SelectedIndex = 0;
+        }
+    }
+}
---
+++ Movie Search App/MovieData.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie_Search_App
+{
+    class MovieData
+    {
+        [JsonProperty("title")]
+        public string Title { get; set; }
+        [JsonProperty("year")]
+        public string Year { get; set; }
+        [JsonProperty("released")]
+        public string Rated { get; set; }
+        [JsonProperty("runtime")]
+        public string Runtime { get; set; }
+        [JsonProperty("released")]
+        public string Released { get; set; }
+        [JsonProperty("genre")]
+        public string Genre { get; set; }
+        [JsonProperty("actors")]
+        public string Actors { get; set; }
+        [JsonProperty("plot")]
+        public string Plot { get; set; }
+        [JsonProperty("language")]
+        public string Language { get; set; }
+        [JsonProperty("country")]
+        public string Country { get; set; }
+        public string Awards { get; set; }
+        public string imdbRating { get; set; }
+        public string imdbID { get; set; }
+        [JsonProperty("director")]
+        public string Director { get; set; }
+        [JsonProperty("Response")]
+        public bool Response { get; set; }
+        [JsonProperty("Error")]
+        public string Error { get; set; }
+    }
+}
